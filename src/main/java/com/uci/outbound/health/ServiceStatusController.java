26991847efd6dc6eee9dd853a1b8116adb0f75ae
Surabhi <surabhi.mahawar@aurigait.com>
2022-07-12 15:20:30 +0530

    readme & api response

--- src/main/java/com/uci/outbound/health/ServiceStatusController.java
+++ src/main/java/com/uci/outbound/health/ServiceStatusController.java
@@ -7,11 +7,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.uci.dao.service.HealthService;
 
+import com.uci.utils.model.ApiResponse;
+import com.uci.utils.model.ApiResponseParams;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,47 +28,39 @@ public class ServiceStatusController {
 	@Autowired 
 	private HealthService healthService;
 
-    @RequestMapping(value = "/health", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-    public ResponseEntity<JsonNode> statusCheck() throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode json = mapper.readTree("{\"id\":\"api.content.service.health\",\"ver\":\"3.0\",\"ts\":null,\"params\":{\"resmsgid\":null,\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"healthy\":true}}");
-        return ResponseEntity.ok(json);
-    }
-    
     @RequestMapping(value = "/health/cassandra", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-    public ResponseEntity<JsonNode> cassandraStatusCheck() throws IOException, JsonProcessingException {
-    	JsonNode jsonNode = getResponseJsonNode();
-    	((ObjectNode) jsonNode).put("result", healthService.getCassandraHealthNode());
-        
-        return ResponseEntity.ok(jsonNode);
+    public ResponseEntity<ApiResponse> cassandraStatusCheck() throws IOException, JsonProcessingException {
+        ApiResponse response = ApiResponse.builder()
+                .id("api.service.health.cassandra")
+                .params(ApiResponseParams.builder().build())
+                .responseCode(HttpStatus.OK.name())
+                .result(healthService.getCassandraHealthNode())
+                .build();
+
+        return ResponseEntity.ok(response);
     }
-    
+
     @RequestMapping(value = "/health/kafka", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-    public ResponseEntity<JsonNode> kafkaStatusCheck() throws IOException, JsonProcessingException {
-    	JsonNode jsonNode = getResponseJsonNode();
-    	((ObjectNode) jsonNode).put("result", healthService.getKafkaHealthNode());
-        
-        return ResponseEntity.ok(jsonNode);
+    public ResponseEntity<ApiResponse> kafkaStatusCheck() throws IOException, JsonProcessingException {
+        ApiResponse response = ApiResponse.builder()
+                .id("api.service.health.kafka")
+                .params(ApiResponseParams.builder().build())
+                .responseCode(HttpStatus.OK.name())
+                .result(healthService.getKafkaHealthNode())
+                .build();
+
+        return ResponseEntity.ok(response);
     }
-    
+
     @RequestMapping(value = "/health/campaign", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-    public ResponseEntity<JsonNode> campaignUrlStatusCheck() throws JsonProcessingException, IOException {
-    	JsonNode jsonNode = getResponseJsonNode();
-        ((ObjectNode) jsonNode).put("result", healthService.getCampaignUrlHealthNode());
-        
-        return ResponseEntity.ok(jsonNode);
-    }
-    
-    /**
-     * Returns json node for service response
-     * 
-     * @return JsonNode
-     * @throws JsonMappingException
-     * @throws JsonProcessingException
-     */
-    private JsonNode getResponseJsonNode() throws JsonMappingException, JsonProcessingException {
-    	ObjectMapper mapper = new ObjectMapper();
-    	JsonNode jsonNode = mapper.readTree("{\"id\":\"api.content.service.health\",\"ver\":\"3.0\",\"ts\":null,\"params\":{\"resmsgid\":null,\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"healthy\":false}}");
-        return jsonNode;
+    public ResponseEntity<ApiResponse> campaignUrlStatusCheck() throws JsonProcessingException, IOException {
+        ApiResponse response = ApiResponse.builder()
+                .id("api.service.health.campaign")
+                .params(ApiResponseParams.builder().build())
+                .responseCode(HttpStatus.OK.name())
+                .result(healthService.getCampaignUrlHealthNode())
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 }
