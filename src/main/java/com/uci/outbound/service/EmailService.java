8f06651343d032437d681cc533b9820583304e47
pankajjangid05 <pankaj.jangid@aurigait.com>
2023-04-04 14:49:51 +0530

    Email config added in properties file for utils

--- src/main/java/com/uci/outbound/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.uci.outbound.service;
-
-import com.uci.outbound.entity.EmailDetails;
-
-public interface EmailService {
-    public void sendSimpleMail(EmailDetails details);
-
-    public void sendMailWithAttachment(EmailDetails details);
-}
