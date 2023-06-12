7173984e125149e0a1cdd2ac0e488902bd8a626e
pankajjangid05 <pankaj.jangid@aurigait.com>
2023-06-12 15:05:44 +0530

    Notification Batch related changes

--- src/main/java/com/uci/outbound/consumers/NotificationConsumerReactive.java
+++ src/main/java/com/uci/outbound/consumers/NotificationConsumerReactive.java
@@ -56,7 +56,7 @@ public class NotificationConsumerReactive {
         try {
             reactiveKafkaReceiverNotification
                     .doOnNext(this::logMessage)
-                    .bufferTimeout(1000, Duration.ofSeconds(10))
+                    .bufferTimeout(500, Duration.ofSeconds(10))
                     .flatMap(this::sendOutboundMessage)
                     .onBackpressureBuffer()
                     .bufferTimeout(1000, Duration.ofSeconds(10))
